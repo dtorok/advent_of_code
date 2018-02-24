dca11522dd731c1ffc09fa227cf391f696b47752
Dani Torok <daniel.torok@uber.com>
2018-02-24 13:55:29 +0100

    [day2] part 1

--- 2017/main.elm
+++ 2017/main.elm
@@ -1,12 +1,15 @@
 module AdventOfCode2017 exposing (main)
 
 import Day1
+import Day2
 import Test
 import Html exposing (..)
 
 main : Html msg
 main = div []
   [ h1 [] [ text "Advent of code 2017" ]
+  , Test.test Day2.part1
+  , Test.test Day2.part2
   , Test.test Day1.part1
   , Test.test Day1.part2
   ]
