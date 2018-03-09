5ea20e50415e1c448d34a1731d0831d579866add
Dani Torok <daniel.torok@uber.com>
2018-03-09 18:45:17 +0100

    [day15] part2

--- 2017/day15.elm
+++ 2017/day15.elm
@@ -6,52 +6,76 @@ import TestRun
 part1 : TestRun.Test
 part1 =
   { title = "Day 15: Dueling Generators - Part 1"
-  , solver = solver
-  , testCases = []
-    -- [ ( "65,8921", "588")
-    -- , ( "618,814", "577")
-    -- ]
+  , solver = solver1
+  , testCases =
+    [ ( "65,8921", "588")
+    , ( "618,814", "577")
+    ]
   }
 
 part2 : TestRun.Test
 part2 =
   { title = "Day 15: Dueling Generators - Part 2"
-  , solver = identity
+  , solver = solver2
   , testCases =
-    [ ("", "")
+    [ ( "65,8921", "309")
+    , ( "618,814", "316")
     ]
   }
 
 -- SOLVER
 ---------
-solver : String -> String
-solver input = input
+solver1 : String -> String
+solver1 input = input
   |> parseInput -- (Int, Int)
-  |> (\(a, b) -> (genNew a 16807, genNew b 48271)) -- (Generator, Generator)
+  |> (\(a, b) -> (genNew a 16807 4, genNew b 48271 8)) -- (Generator, Generator)
   -- |> compareGens 10
-  |> compareGens 40000000
+  |> compareGens genStep 40000000
+  |> toString
+
+solver2 : String -> String
+solver2 input = input
+  |> parseInput
+  |> (\(a, b) -> (genNew a 16807 4, genNew b 48271 8)) -- (Generator, Generator)
+  |> compareGens genStepMultiplied 5000000
   |> toString
 
 type alias Generator =
   { current : Int
   , factor : Int
+  , multiplier : Int
   }
 
-genNew : Int -> Int -> Generator
-genNew current factor =
+genNew : Int -> Int -> Int -> Generator
+genNew current factor multiplier =
   { current = current
   , factor = factor
+  , multiplier = multiplier
   }
 
 genStep : Generator -> Generator
 genStep gen =
   { gen | current = (gen.current * gen.factor) % 2147483647 }
 
+genMultiplied : Generator -> Bool
+genMultiplied gen =
+  gen.current % gen.multiplier == 0
+
+genStepMultiplied : Generator -> Generator
+genStepMultiplied gen =
+  let
+    newGen = genStep gen
+  in
+    if genMultiplied newGen then
+      newGen
+    else
+      genStepMultiplied newGen
+
 genLow16 : Generator -> Int
 genLow16 gen = gen.current % 65536
 
-compareGens : Int -> (Generator, Generator) -> Int
-compareGens stepCount gens =
+compareGens : (Generator -> Generator) -> Int -> (Generator, Generator) -> Int
+compareGens fStepper stepCount gens =
   let
     step : Int -> (Generator, Generator) -> Int -> Int
     step stepCount (genA, genB) cnt =
@@ -60,8 +84,8 @@ compareGens stepCount gens =
           gensMatch = (genLow16 genA) == (genLow16 genB)
           newCnt = if gensMatch then cnt + 1 else cnt
 
-          newGenA = genStep genA
-          newGenB = genStep genB
+          newGenA = fStepper genA
+          newGenB = fStepper genB
         in
           -- Debug.log ("step: " ++ (toString (genA, genB)))<|
           step (stepCount - 1) (newGenA, newGenB) newCnt
