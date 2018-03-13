efeeaed23baa4a40e2e62873de06d5016dffc96b
Dani Torok <daniel.torok@uber.com>
2018-03-13 21:50:49 +0100

    [day17] part 1 and 2

--- 2017/day17.elm
+++ 2017/day17.elm
@@ -6,41 +6,67 @@ import Dict exposing (Dict)
 part1 : TestRun.Test
 part1 =
   { title = "Day 17: Spinlock - Part 1"
-  , solver = solver
+  , solver = solver1
   , testCases =
-    [ ( "3", "638")
-    , ( "303", "1971")
+    [ ( "3,2017", "638")
+    , ( "303,2017", "1971")
     ]
   }
 
 part2 : TestRun.Test
 part2 =
   { title = "Day 17: Spinlock - Part 2"
-  , solver = identity
+  , solver = solver2
   , testCases =
-    [ ("", "")
+    [ ("3,10", "9")
+    , ("303,50000000", "17202899")
     ]
   }
 
 -- SOLVER
 ---------
-solver : String -> String
-solver input =
+solver1 : String -> String
+solver1 input =
   let
-    stepCount : Int
-    stepCount = Result.withDefault 0 (String.toInt input)
+    (stepCount, numRounds) = parseInput input
   in
-    List.range 1 2017
+    List.range 1 numRounds
       |> List.foldl (runRound stepCount) (0, ringNew)
-      |> (\(curr, ring) -> unsafeGet 2017 ring)
+      |> (\(curr, ring) -> unsafeGet numRounds ring)
       |> toString
 
+solver2 : String -> String
+solver2 input =
+  let
+    (stepCount, numRounds) = parseInput input
+
+    folder : Int -> (Int, Int, Int) -> (Int, Int, Int)
+    folder i (len, curr, result) =
+      let
+        newLen = len + 1
+        newCurr = (curr + stepCount) % len + 1
+        newResult = if newCurr == 1 then i else result
+      in
+        (newLen, newCurr, newResult)
+
+    f : Int -> (Int, Int, Int) -> Int
+    f num (len, curr, result) =
+      if num < numRounds then
+        f
+          (num + 1)
+          (folder num (len, curr, result))
+      else
+        result
+  in
+    toString <| f 1 (1, 0, -1)
 
 type alias Ring = Dict Int Int
 
 runRound : Int -> Int -> (Int, Ring) -> (Int, Ring)
 runRound stepCount newVal (current, ring) =
   let
+    nomrStepCount = stepCount % (Dict.size ring)
+
     f : Int -> Int -> Int
     f _ curr = ringNext ring curr
 
@@ -66,3 +92,19 @@ ringInsertRight : Int -> Int -> Ring -> Ring
 ringInsertRight index value ring = ring
   |> Dict.insert value (unsafeGet index ring)
   |> Dict.insert index value
+
+-- PARSER
+---------
+parseInput : String -> (Int, Int)
+parseInput input = input
+  |> String.split ","
+  |> List.map String.toInt
+  |> List.map (Result.mapError (\e -> Debug.crash("Couldn't parse ints: " ++ (toString e) ++ " " ++ (toString input))))
+  |> List.map (Result.withDefault 0)
+  |> list2Tuple
+
+list2Tuple : List Int -> (Int, Int)
+list2Tuple l =
+  case l of
+    a :: b :: xs -> (a, b)
+    _ -> Debug.crash ("Not enough values for tuple: " ++ (toString l))
