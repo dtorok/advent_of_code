f656e458c08ee1b82a42409136ab85e5ebd8a3e4
Dani Torok <daniel.torok@uber.com>
2018-03-18 21:20:11 +0100

    [day19] part1

--- 2017/main.elm
+++ 2017/main.elm
@@ -21,46 +21,49 @@ import Day15
 import Day16
 import Day17
 import Day18
+import Day19
 
 main : Html msg
 main = div []
   [ h1 [] [ text "Advent of code 2017" ]
-  , TestRun.test Day18.part1
-  , TestRun.test Day18.part2
-  , TestRun.test Day17.part1
-  , TestRun.test Day17.part2
-  , TestRun.test Day16.part1
-  , TestRun.test Day16.part2
-  , TestRun.test Day15.part1
-  , TestRun.test Day15.part2
-  , TestRun.test Day14.part1
-  , TestRun.test Day14.part2
-  , TestRun.test Day13.part1
-  , TestRun.test Day13.part2
-  , TestRun.test Day12.part1
-  , TestRun.test Day12.part2
-  , TestRun.test Day11.part1
-  , TestRun.test Day11.part2
-  , TestRun.test Day10.part1
-  , TestRun.test Day10.part2
-  , TestRun.test Day09.part1
-  , TestRun.test Day09.part2
-  , TestRun.test Day08.part1
-  , TestRun.test Day08.part2
-  , TestRun.test Day07.part1
-  , TestRun.test Day07.part2
-  , TestRun.test Day06.part1
-  , TestRun.test Day06.part2
-  , TestRun.test Day05.part1
-  , TestRun.test Day05.part2
-  , TestRun.test Day04.part1
-  , TestRun.test Day04.part2
-  , TestRun.test Day03.part1
-  , TestRun.test Day03.part2
-  , TestRun.test Day02.part1
-  , TestRun.test Day02.part2
-  , TestRun.test Day01.part1
-  , TestRun.test Day01.part2
+  , TestRun.test Day19.part1
+  , TestRun.test Day19.part2
+  -- , TestRun.test Day18.part1
+  -- , TestRun.test Day18.part2
+  -- , TestRun.test Day17.part1
+  -- , TestRun.test Day17.part2
+  -- , TestRun.test Day16.part1
+  -- , TestRun.test Day16.part2
+  -- , TestRun.test Day15.part1
+  -- , TestRun.test Day15.part2
+  -- , TestRun.test Day14.part1
+  -- , TestRun.test Day14.part2
+  -- , TestRun.test Day13.part1
+  -- , TestRun.test Day13.part2
+  -- , TestRun.test Day12.part1
+  -- , TestRun.test Day12.part2
+  -- , TestRun.test Day11.part1
+  -- , TestRun.test Day11.part2
+  -- , TestRun.test Day10.part1
+  -- , TestRun.test Day10.part2
+  -- , TestRun.test Day09.part1
+  -- , TestRun.test Day09.part2
+  -- , TestRun.test Day08.part1
+  -- , TestRun.test Day08.part2
+  -- , TestRun.test Day07.part1
+  -- , TestRun.test Day07.part2
+  -- , TestRun.test Day06.part1
+  -- , TestRun.test Day06.part2
+  -- , TestRun.test Day05.part1
+  -- , TestRun.test Day05.part2
+  -- , TestRun.test Day04.part1
+  -- , TestRun.test Day04.part2
+  -- , TestRun.test Day03.part1
+  -- , TestRun.test Day03.part2
+  -- , TestRun.test Day02.part1
+  -- , TestRun.test Day02.part2
+  -- , TestRun.test Day01.part1
+  -- , TestRun.test Day01.part2
   ]
 
 testMain : Html msg
