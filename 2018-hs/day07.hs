05a2d5d8a11e20dcc13639db4ee1ca07340904a0
Daniel Torok <dani@danisoft.hu>
2019-02-03 21:42:03 +0000

    2018-day07-hs 2nd part + cleanup

--- 2018-hs/day07.hs
+++ 2018-hs/day07.hs
@@ -1,9 +1,11 @@
 import Utils
 
 import Text.ParserCombinators.ReadP
+import Data.List
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
--- import Debug.Trace
+import Data.Char
+import Debug.Trace
 
 type Step = (Char, Char)
 type Deps = Map.Map Char (Set.Set Char)
@@ -20,22 +22,48 @@ mkDeps steps = foldl addDep Map.empty steps
       $ Map.insertWith (Set.union) second (Set.singleton first)
       $ deps
 
-solve1 :: String -> String
-solve1 input = show . go "" . mkDeps . parse parseStep . lines $ input
+readyToDo :: Deps -> [Char]
+readyToDo deps = Map.keys . Map.filter (== Set.empty) $ deps
+
+cleanDep :: Char -> Deps -> Deps
+cleanDep d = Map.map (Set.delete d)
+
+solveGen :: Int -> String -> String
+solveGen num input = show . go "" Map.empty 0 . mkDeps . parse parseStep . lines $ input
   where
-    go :: String -> Deps -> String
-    go result deps
-      | Map.null deps = reverse result
-      | otherwise     = go (next:result) deps'
+    go :: String -> Map.Map Char Int -> Int -> Deps -> (String, Int)
+    go result work ts deps
+      | Map.null work && Map.null deps = (reverse result, ts - 1)
+      | otherwise = go result' work' (ts + 1) deps'
         where
-          next :: Char
-          next = minimum . Map.keys . Map.filter (== Set.empty) $ deps
+          workLength :: Char -> Int
+          workLength c = 60 + ord c - ord 'A' + 1
+
+          finished :: [Char]
+          finished = Map.keys . Map.filter (<= ts) $ work
+
+          depsWoFinished :: Deps
+          depsWoFinished = foldl (flip cleanDep) deps finished
+
+          workWoFinished :: Map.Map Char Int
+          workWoFinished = foldl (flip Map.delete) work finished
+
+          toStart :: [Char]
+          toStart = take (num - Map.size work + length finished) . readyToDo $ depsWoFinished
+
+          workToStart :: Map.Map Char Int
+          workToStart = Map.fromList . map (\c -> (c, ts + workLength c)) $ toStart
+
+          work' :: Map.Map Char Int
+          work' = workWoFinished `Map.union` workToStart
 
           deps' :: Deps
-          deps' = Map.delete next
-                $ Map.map (Set.delete next)
-                $ deps
+          deps' = foldl (flip Map.delete) depsWoFinished toStart
+
+          result' :: String
+          result' = (reverse . sort $ finished) ++ result
 
 main :: IO ()
 main = do
-  solve "../2018/day7.input" "07/01" solve1
+  solve "../2018/day7.input" "07/01" (solveGen 1)
+  solve "../2018/day7.input" "07/02" (solveGen 5)
