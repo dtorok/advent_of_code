6b70a6eb3a1b45beaee8c392f3962b8ad582fadd
Daniel Torok <dani@danisoft.hu>
2019-02-06 20:37:45 +0000

    day08 part2

--- 2018-hs/day08.hs
+++ 2018-hs/day08.hs
@@ -1,7 +1,7 @@
 import Utils
 
 import Text.ParserCombinators.ReadP
-
+import Data.Maybe
 import Debug.Trace
 
 
@@ -25,13 +25,30 @@ parseLicenseNode = do
     , children    = children
     , metadata    = metadata }
 
+at :: [a] -> Int -> Maybe a
+at [] _ = Nothing
+at (x:_) 0 = Just x
+at xs i = at (drop i xs) 0
+
 sumMetadata :: LicenseNode -> Int
 sumMetadata l = sumMeta + sumRecurMeta
   where
     sumMeta = sum . metadata $ l
     sumRecurMeta = sum . map sumMetadata . children $ l
 
+calcValue :: LicenseNode -> Int
+calcValue l
+  | numChildren l == 0 = sumMetadata l
+  | otherwise          = sum . map calcValue . mapMaybe findChild . metadata $ l
+    where
+      findChild i = (children l) `at` (i - 1)
+
 solve1 :: String -> String
 solve1 input = show . sumMetadata . fst . head . readP_to_S parseLicenseNode $ input
 
-main = solve "../2018/day8.input" "08/01" solve1
+solve2 :: String -> String
+solve2 input = show . calcValue . fst . head . readP_to_S parseLicenseNode $ input
+
+main = do
+  solve "../2018/day8.input" "08/01" solve1
+  solve "../2018/day8.input" "08/02" solve2
