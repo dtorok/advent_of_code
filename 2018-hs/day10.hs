8ab861330214bec682ae469dc5c3245550fb2cec
Daniel Torok <dani@danisoft.hu>
2019-02-16 00:33:43 +0000

    cleanup

--- 2018-hs/day10.hs
+++ 2018-hs/day10.hs
@@ -4,67 +4,59 @@ import Text.ParserCombinators.ReadP
 import qualified Data.List as L
 import Debug.Trace
 
-data Point = Point Int Int Int Int deriving (Show)
+data V = V { y :: Int, x:: Int} deriving (Eq, Ord)
+data Star = Star V V
 
-instance Ord Point where
-  (<=) (Point x1 y1 _ _) (Point x2 y2 _ _)
-    | y1 == y2  = x1 <= x2
-    | otherwise = y1 <= y2
+parseStar :: ReadS Star
+parseStar = readP_to_S $ do
+  x  <- string "position=<" >>> parseNextNumber
+  y  <- string ", " >>> parseNextNumber
+  vx <- string "> velocity=<" >>> parseNextNumber
+  vy <- string ", " >>> parseNextNumber
+  return $ Star (V y x) (V vy vx)
 
-instance Eq Point where
-  (==) (Point x1 y1 _ _) (Point x2 y2 _ _) = x1 == x2 && y1 == y2
+coord :: Star -> V
+coord (Star v _) = v
 
-parsePoint :: ReadS Point
-parsePoint = readP_to_S $ Point
-  <$> string "position=<" >>> parseNextNumber
-  <*> string ", " >>> parseNextNumber
-  <*> string "> velocity=<" >>> parseNextNumber
-  <*> string ", " >>> parseNextNumber
-
-getx :: Point -> Int
-getx (Point x _ _ _ ) = x
-
-gety :: Point -> Int
-gety (Point _ y _ _ ) = y
-
-bb :: [Point] -> ((Int, Int), (Int, Int))
-bb points = ((minx, miny), (maxx - minx, maxy - miny))
+bb :: [V] -> (V, V)
+bb vectors = (V miny minx, V (maxy - miny) (maxx - minx))
   where
-    xs = map getx points
-    ys = map gety points
+    xs = map x vectors
+    ys = map y vectors
     minx = minimum xs
     miny = minimum ys
     maxx = maximum xs
     maxy = maximum ys
 
-step :: Point -> Point
-step (Point x y vx vy) = Point (x + vx) (y + vy) vx vy
+step :: Star -> Star
+step (Star (V y x) (V vy vx)) = Star (V (y + vy) (x + vx)) (V vy vx)
 
-draw :: [Point] -> String
-draw points = go sorted (minx, miny) "\n"
+draw :: [V] -> String
+draw vectors = go sorted (minx, miny) "\n"
   where
-    sorted = L.nub . L.sort $ points
-    ((minx, miny), (w, h)) = bb points
-    go [] _ result = reverse result
-    go points@((Point px py _ _):xs) (x, y) result
-      | x > minx + w       = go points (minx, y + 1) ('\n' : result)
-      | x == px && y == py = go xs     (x + 1, y)    ('X'  : result)
+    sorted               = L.nub . L.sort $ vectors
+    (V miny minx, V h w) = bb vectors
+
+    go [] _ result         = reverse result
+    go vs@((V py px):xs) (x, y) result
       | y > miny + h       = undefined
-      | otherwise          = go points (x + 1, y)    ('.'  : result)
+      | x > minx + w       = go vs (minx, y + 1) ('\n' : result)
+      | x == px && y == py = go xs (x + 1, y)    ('X'  : result)
+      | otherwise          = go vs (x + 1, y)    ('.'  : result)
 
-moveStars :: Int -> Int -> [Point] -> (Int, [Point])
-moveStars w i points | w == 0    = moveStars (fst . snd . bb $ points) (i+1) points
-                     | w >= w'   = moveStars w' (i+1) points'
-                     | otherwise = (i - 1, points)
+moveStars :: Int -> Int -> [Star] -> (Int, [Star])
+moveStars w i stars | w == 0    = moveStars (x . snd . bb . map coord $ stars) (i+1) stars
+                    | w >= w'   = moveStars w' (i+1) stars'
+                    | otherwise = (i - 1, stars)
   where
-    points' = map step points
-    w' = fst . snd . bb $ points'
+    stars' = map step stars
+    w' = x . snd . bb . map coord $ stars'
 
 solve1 :: String -> String
-solve1 = draw . snd . moveStars 0 0 . parse parsePoint . lines
+solve1 = draw . map coord . snd . moveStars 0 0 . parse parseStar . lines
 
 solve2 :: String -> String
-solve2 = show . fst . moveStars 0 0 . parse parsePoint . lines
+solve2 = show . fst . moveStars 0 0 . parse parseStar . lines
 
 main :: IO ()
 main = do
