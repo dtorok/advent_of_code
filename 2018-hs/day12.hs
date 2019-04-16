44f6fcc5af258a7c5729a0899460ebcaeb1179e8
Daniel Torok <dani@danisoft.hu>
2019-04-16 22:42:40 +0100

    2018/12 use String instead of Seq

--- 2018-hs/day12.hs
+++ 2018-hs/day12.hs
@@ -3,25 +3,24 @@ import Utils
 import Text.ParserCombinators.ReadP
 import Data.Char
 import qualified Data.Map.Strict as Map
-import qualified Data.Sequence as Seq
 import qualified Data.Foldable as Foldable
 import Debug.Trace
 
-data Plants = Plants Int (Seq.Seq Char) deriving Show
+data Plants = Plants Int String deriving Show
 type Rules = Map.Map String Char
+
 
 parseRule :: ReadP (String, Char)
 parseRule = (,)
   <$> count 5 get <<< string " => "
   <*> get
 
--- parseInput :: ReadP (String, [(String, Char)])
 parseInput :: ReadP (Plants, Rules)
 parseInput = do
   plantsString <- string "initial state: " >>> many (char '.' +++ char '#') <<< string "\n\n"
   rulesList <- many1 (parseRule <<< char '\n') <<< eof
 
-  let plants = Plants 0 (Seq.fromList plantsString)
+  let plants = Plants 0 plantsString
   let rules = Map.fromList rulesList
 
   return (plants, rules)
@@ -33,16 +32,16 @@ extend :: Plants -> Plants
 extend (Plants z p) = Plants z' p'
   where
     z' = z - 5
-    p' = (Seq.fromList ".....") Seq.>< p Seq.>< (Seq.fromList ".....")
+    p' = "....." ++ p
 
 reduce :: Plants -> Plants
 reduce (Plants z p) = Plants z' p'
   where
-    z' = z + Seq.length (Seq.takeWhileL emptypot p)
-    p' = Seq.dropWhileL emptypot . Seq.dropWhileR emptypot $ p
+    z' = z + length (takeWhile emptypot p)
+    p' = dropWhile emptypot p
 
 calcResult :: Plants -> Int
-calcResult (Plants z p) = go z (Foldable.toList p) 0
+calcResult (Plants z p) = go z p 0
   where
     go _ [] res = res
     go i ('.':xs) res = go (i + 1) xs res
@@ -60,18 +59,17 @@ solve1 gens input = show . calcResult . foldl growGen initplants $ [1..gens]
     growPlants (Plants z p) = Plants z' p'
       where
         z' = z + 2
-        p' = grow Seq.empty p
+        p' = reverse . grow "" $ p
 
-    grow :: Seq.Seq Char -> Seq.Seq Char -> Seq.Seq Char
-    grow result pots@(p Seq.:<| ps)
-      | Seq.length pots < 5 = result
-      | otherwise           = grow result' ps
-        where
-          result' = result Seq.|> Map.findWithDefault '.' pattern rules
-          pattern = Foldable.toList . Seq.take 5 $ pots
+    grow :: String -> String -> String
+    grow result ('.':'.':'.':'.':'.':[]) = result
+    grow result (a:b:c:d:[]) = grow result (a:b:c:d:'.':[])
+    grow result (a:b:c:d:e:xs) = grow (curr : result) (b:c:d:e:xs)
+      where
+        curr = Map.findWithDefault '.' (a:b:c:d:e:[]) rules
 
 main :: IO ()
 main = do
   solve "../2018/day12.input" "12/01" (solve1 20)
   solve "../2018/day12.input" "12/02" (solve1 500)
-  putStrLn . show . calcResult $ Plants (50000000000 - 95) (Seq.fromList "#.#.....#.#.....#.#.....#.#.....#.#.....#.#.....#.#............#.#.....#.#.....#.#.....#.#.....#.#.....#.#.....#.#.....#.#......#.#.....#.#........#.#.....#.#.......#.#...#.#.....#.#......#.#")
+  putStrLn . show . calcResult $ Plants (50000000000 - 95) "#.#.....#.#.....#.#.....#.#.....#.#.....#.#.....#.#............#.#.....#.#.....#.#.....#.#.....#.#.....#.#.....#.#.....#.#......#.#.....#.#........#.#.....#.#.......#.#...#.#.....#.#......#.#"
